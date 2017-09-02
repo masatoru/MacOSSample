dcce406182f61b6066f9b7718910bd1a435839c6
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-09-02 10:35:08 +0900

    Add NotificatoinCenter

--- NSTableViewSample/ViewController.cs
+++ NSTableViewSample/ViewController.cs
@@ -22,6 +22,11 @@ namespace NSTableViewSample
             tableView.Delegate = new PersonTableDelegate(ds);
             tableView.DataSource = ds;
 
+            // 送信者を指定して購読者を指定する
+            NSNotificationCenter.DefaultCenter
+                                .AddObserver(NSTableView.SelectionDidChangeNotification,
+                                            PersonSelected,tableView);
+
             PersonList.Add(new Person { Name = "Ichiro", Mail = "ichiro@marines.com" });
             PersonList.Add(new Person { Name = "Darvish", Mail = "darvish@dogers.com" });
 
@@ -29,6 +34,20 @@ namespace NSTableViewSample
 
             // Do any additional setup after loading the view.
         }
+
+        private void PersonSelected(NSNotification obj){
+            var index = tableView.SelectedRow;
+            if (index < 0) return;
+            var item =PersonList[(int)index];
+
+			using (var alert = new NSAlert())
+			{
+                alert.MessageText = $"Name={item.Name} Mail={item.Mail}";
+				alert.RunSheetModal(null);
+				return;
+			}
+
+		}
 
         public override NSObject RepresentedObject
         {
